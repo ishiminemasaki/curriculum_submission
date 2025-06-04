a9c07bff637e5b89de2da8ae79caf0568babeb88
tthjp <tthjp@note>
2025-06-04 10:10:37 +0900

    Java1.8

--- Java_1.8/src/curriculum_A/Qes1_13.java
+++ Java_1.8/src/curriculum_A/Qes1_13.java
@@ -11,22 +11,14 @@ public class Qes1_13 {
         double weight = 64.2;  // kg
         String favoriteFood = "オムライス";
 
-        // 自己紹介の出力
-        System.out.println("初めまして" + name + "です");
-        System.out.println("年齢は" + age + "歳です");
-        System.out.println("身長" + height + "cmです");
-        System.out.println("体重は" + weight + "kgです");
-        System.out.println("好きな食べ物は" + favoriteFood + "です");
+        // 数値を文字列に型変換
+        String ageStr = String.valueOf(age);
+        String heightStr = String.valueOf(height);
+        String weightStr = String.valueOf(weight);
 
-        // BMI計算
-        double heightInMeters = height / 100;
-        double bmi = weight / (heightInMeters * heightInMeters);
+        // 文字列として連結して出力
+        System.out.println(ageStr + heightStr + weightStr);
         
-        System.out.printf("BMIは%.2fです\n", bmi);
-
-        // 年齢が25歳以上かどうか出力
-        System.out.println(age >= 25);
-
 	}
 
 }
