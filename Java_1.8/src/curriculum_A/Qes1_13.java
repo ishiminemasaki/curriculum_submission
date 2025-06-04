2fb47b61d8247e85e23ce21b9c83507258a74f88
tthjp <tthjp@note>
2025-06-04 09:18:56 +0900

    Java1.8

---
+++ Java_1.8/src/curriculum_A/Qes1_13.java
@@ -0,0 +1,35 @@
+package curriculum_A;
+
+public class Qes1_13 {
+
+	public static void main(String[] args) {
+		byte byteVar;
+		// バイト型
+		
+        short shortVar;
+        // 短整数型
+        
+        int intVar;
+        // 整数型
+        
+        long longVar;
+        // 長整数型
+        
+        float floatVar;
+        // 単精度浮動小数点数型
+        
+        double doubleVar;
+        // 倍精度浮動小数点数型
+        
+        char charVar;
+        // 文字型
+        
+        String stringVar;
+        // 文字列型
+        
+        boolean booleanVar;
+        // ブーリアン型
+
+	}
+
+}
