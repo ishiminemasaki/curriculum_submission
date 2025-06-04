7fa20291f4decc2dd98a01a78cb40bdfa5bcdd4b
tthjp <tthjp@note>
2025-06-04 09:26:31 +0900

    Java1.8

--- Java_1.8/src/curriculum_A/Qes1_13.java
+++ Java_1.8/src/curriculum_A/Qes1_13.java
@@ -3,32 +3,33 @@ package curriculum_A;
 public class Qes1_13 {
 
 	public static void main(String[] args) {
-		byte byteVar;
+		
+		byte byteVar = 0;
 		// バイト型
 		
-        short shortVar;
+        short shortVar = 0;
         // 短整数型
         
-        int intVar;
+        int intVar = 0;
         // 整数型
         
-        long longVar;
-        // 長整数型
+        long longVar = 0L;
+        // 長整数型（末尾にLを付ける）
         
-        float floatVar;
-        // 単精度浮動小数点数型
+        float floatVar = 0.0f;
+        // 単精度浮動小数点数型（末尾にfを付ける）
         
-        double doubleVar;
+        double doubleVar = 0.0;
         // 倍精度浮動小数点数型
         
-        char charVar;
-        // 文字型
+        char charVar = '\u0000';
+        // 文字型（charの初期値はヌル文字）
         
-        String stringVar;
-        // 文字列型
+        String stringVar = "";
+        // 文字列型（空文字で初期化）
         
-        boolean booleanVar;
-        // ブーリアン型
+        boolean booleanVar = false;
+        // ブーリアン型（trueまたはfalse）
 
 	}
 
