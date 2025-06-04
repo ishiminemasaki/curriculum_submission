f9a30798de62248e4478f813c59de4e434e00024
tthjp <tthjp@note>
2025-06-04 09:49:34 +0900

    Java1.8

--- Java_1.8/src/curriculum_A/Qes1_13.java
+++ Java_1.8/src/curriculum_A/Qes1_13.java
@@ -4,26 +4,25 @@ public class Qes1_13 {
 
 	public static void main(String[] args) {
 		
-        // 『山田太郎 18歳 170.5cm 62.2kg 寿司』の情報を変数に代入
-        String name = "山田太郎";
-        int age = 18;
-        double height = 170.5;
-        double weight = 62.2;
-        String favoriteFood = "寿司";
+        // 一度宣言した変数に再代入
+        String name = "鈴木一郎";
+        int age = 24;
+        double height = 168.5; // cm
+        double weight = 64.2;  // kg
+        String favoriteFood = "オムライス";
 
-        // フォーマットに従って出力
+        // 自己紹介の出力
         System.out.println("初めまして" + name + "です");
         System.out.println("年齢は" + age + "歳です");
-        System.out.println("身長は" + height + "cmです");
+        System.out.println("身長" + height + "cmです");
         System.out.println("体重は" + weight + "kgです");
         System.out.println("好きな食べ物は" + favoriteFood + "です");
-        
-        // BMI 計算： 身長(cm)をmに変換してから計算
+
+        // BMIの計算と出力（小数第1位まで表示）
         double heightInMeters = height / 100;
         double bmi = weight / (heightInMeters * heightInMeters);
 
-        // BMI 出力（小数第2位まで表示したい場合）
-        System.out.printf("BMIは%.2fです\n", bmi);
+        System.out.printf("BMIは%.1fです\n", bmi);
 
 	}
 
