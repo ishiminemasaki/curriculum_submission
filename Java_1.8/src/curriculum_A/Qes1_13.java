78e116bcc8fc392e6f1623a66e28e2582c1eb6ff
tthjp <tthjp@note>
2025-06-04 09:43:09 +0900

    Java1.8

--- Java_1.8/src/curriculum_A/Qes1_13.java
+++ Java_1.8/src/curriculum_A/Qes1_13.java
@@ -4,13 +4,19 @@ public class Qes1_13 {
 
 	public static void main(String[] args) {
 		
-        String num = "20";
-        int num1 = 23;
+        // 『山田太郎 18歳 170.5cm 62.2kg 寿司』の情報を変数に代入
+        String name = "山田太郎";
+        int age = 18;
+        double height = 170.5;
+        double weight = 62.2;
+        String favoriteFood = "寿司";
 
-        // 文字列 "20" を int に変換して加算
-        int result = Integer.parseInt(num) + num1;
-
-        System.out.println("ハローJAVA" + result); // ハローJAVA43
+        // フォーマットに従って出力
+        System.out.println("初めまして" + name + "です");
+        System.out.println("年齢は" + age + "歳です");
+        System.out.println("身長は" + height + "cmです");
+        System.out.println("体重は" + weight + "kgです");
+        System.out.println("好きな食べ物は" + favoriteFood + "です");
 
 	}
 
